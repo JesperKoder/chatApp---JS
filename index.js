ca1bd7314e3f1c3b7f425f7d47799e1b6eaeae35
Jesper Stene <131758777+JesperKoder@users.noreply.github.com>
2024-07-29 14:54:39 +0200

    got it connected now.

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 import { Server } from "socket.io";
-import { createServer } from 'node:http';
+import { createServer } from 'http';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 
 const app = express();
 const server = createServer(app);
-
+const io = new Server(server);
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Serve socket.io client script
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 server.listen(3000, () => {
