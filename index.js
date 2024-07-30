a41e1994a25379c9d4f5394cfd32909974fbfee9
Jesper Stene <131758777+JesperKoder@users.noreply.github.com>
2024-07-30 14:19:07 +0200

    connecting to sqlite so i can save messages

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ import { createServer } from 'http';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+});
+
+await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Serve socket.io client script
