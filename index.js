34dba982c7d07004f044a1f2cd3486b80dca47e0
Jesper Stene <131758777+JesperKoder@users.noreply.github.com>
2024-07-30 10:14:48 +0200

    messages show up now and are styled.

--- index.js
+++ index.js
@@ -18,7 +18,17 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  })
 });
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
 
 server.listen(3000, () => {
     console.log('Server running on PORT:3000');
